2711669ecbdfabb0950bf4644eefc48f2908f59b
alailsonko <aalailson3@gmail.com>
2021-05-04 11:37:48 -0300

    switch in php

---
+++ beginner/phpbasics/switch.php
@@ -0,0 +1,17 @@
+<?php
+   $favcolor = "red";
+
+   switch ($favcolor) {
+       case "red":
+            echo "Your fav color is red";
+            break;
+        case "blue":
+            echo "Your fav color is red";
+            break;
+        case "green":
+            echo "Your fav color is red";
+            break;
+        default:
+            echo "Your favorite color is neither red, blue, nor green";
+   }
+?>
